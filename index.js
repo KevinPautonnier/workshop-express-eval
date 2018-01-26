c34f4d6b700dc7da86440aac10ae1d731e78e8b6
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-01-26 15:13:42 +0100

    Exercise 3

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = require('./src/app');
 const port = process.argv[2];
 
 // start server here
+app.get('/bingo', function(req, res) {
+    res.end('Bingo')
+  })
 app.listen(port)
 
 console.log(chalk.green('Hello web server on port '+port));
