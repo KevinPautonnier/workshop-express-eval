c2d3b3bff00386e0507b79dfda9f027dcf6cc51f
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-01-26 15:30:06 +0100

    Exercise 4

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 var express = require('express')
+var fs = require('fs')
 
 const app = express();
+var numbers = fs.readFileSync('src/numbers.txt').toString();
+
+numbers = numbers.split("\r\n").reverse().slice(1).reverse().join(', ');
+
+app.get('/bingo', function(req, res) {
+    res.end(numbers)
+  })
 
 module.exports = app;
