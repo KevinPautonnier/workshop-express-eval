09d1439d6a068b88254418ce75863ba0cd0fe0eb
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-01-26 15:58:42 +0100

    Exercise 5

--- src/app.js
+++ src/app.js
@@ -7,7 +7,17 @@ var numbers = fs.readFileSync('src/numbers.txt').toString();
 numbers = numbers.split("\r\n").reverse().slice(1).reverse().join(', ');
 
 app.get('/bingo', function(req, res) {
-    res.end(numbers)
+    if (req.query.mynumbers != undefined){
+        if(numbers.split(', ').join(',') == req.query.mynumbers){
+            res.send('Bingo')
+        }
+        else{
+            res.send('The bingo game is already started, sorry your numbers doesn\'t match with known numbers '+numbers+' so you can not say Bingo')
+        }
+    }
+    else{
+        res.send('The bingo game is already started and known numbers are '+numbers)
+    }
   })
 
 module.exports = app;
